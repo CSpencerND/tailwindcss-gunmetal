c2518cc72959d074c8d9871b40cdb6a0edf438f2
cspencer <cspencernd@gmail.com>
2024-05-24 11:56:20 -0400

    build the project

---
+++ dist/index.d.mts
@@ -0,0 +1,8 @@
+import * as tailwindcss_types_config from 'tailwindcss/types/config';
+
+declare const _default: {
+    handler: tailwindcss_types_config.PluginCreator;
+    config?: Partial<tailwindcss_types_config.Config> | undefined;
+};
+
+export { _default as default };
---
+++ dist/index.js
@@ -0,0 +1,58 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  default: () => src_default
+});
+module.exports = __toCommonJS(src_exports);
+var import_plugin = __toESM(require("tailwindcss/plugin"));
+var src_default = (0, import_plugin.default)(() => {
+}, {
+  theme: {
+    extend: {
+      colors: {
+        gunmetal: {
+          50: "#d8dee3",
+          100: "#bec7cf",
+          200: "#a5b0bb",
+          300: "#8e99a7",
+          400: "#788392",
+          500: "#646e7e",
+          600: "#50596a",
+          700: "#3e4656",
+          800: "#2e3342",
+          900: "#1e222e",
+          950: "#10121a"
+        }
+      }
+    }
+  }
+});
