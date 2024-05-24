43f9e3d0cc98c6c4bae317431c7f409168983be9
cspencer <cspencernd@gmail.com>
2024-05-24 11:49:41 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import plugin from "tailwindcss/plugin";
+
+export default plugin(() => { }, {
+    theme: {
+        extend: {
+            colors: {
+                gunmetal: {
+                    50: "#d8dee3",
+                    100: "#bec7cf",
+                    200: "#a5b0bb",
+                    300: "#8e99a7",
+                    400: "#788392",
+                    500: "#646e7e",
+                    600: "#50596a",
+                    700: "#3e4656",
+                    800: "#2e3342",
+                    900: "#1e222e",
+                    950: "#10121a",
+                },
+            },
+        },
+    },
+});
