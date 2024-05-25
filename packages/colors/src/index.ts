1f0841d5555c374cce06718fb9591edcf8820960
cspencer <cspencernd@gmail.com>
2024-05-25 13:02:22 -0400

    trying to get this to work correctly with typescript

--- packages/colors/src/index.ts
+++ packages/colors/src/index.ts
@@ -1,23 +1,28 @@
 import plugin from "tailwindcss/plugin";
+import type { Config } from "tailwindcss";
 
-export default plugin(() => { }, {
+export const colors = {
+    50: "#d8dee3",
+    100: "#bec7cf",
+    200: "#a5b0bb",
+    300: "#8e99a7",
+    400: "#788392",
+    500: "#646e7e",
+    600: "#50596a",
+    700: "#3e4656",
+    800: "#2e3342",
+    900: "#1e222e",
+    950: "#10121a",
+};
+
+const config: Partial<Config> = {
     theme: {
         extend: {
             colors: {
-                gunmetal: {
-                    50: "#d8dee3",
-                    100: "#bec7cf",
-                    200: "#a5b0bb",
-                    300: "#8e99a7",
-                    400: "#788392",
-                    500: "#646e7e",
-                    600: "#50596a",
-                    700: "#3e4656",
-                    800: "#2e3342",
-                    900: "#1e222e",
-                    950: "#10121a",
-                },
+                gunmetal: colors,
             },
         },
     },
-});
+};
+
+export default plugin(() => { }, config);
--- packages/shadcn/src/index.ts
+++ packages/shadcn/src/index.ts
@@ -1,3 +1,68 @@
-import colors from "@tailwindcss-gunmetal/colors";
+// TODO: Turn this file into a plugin so you can export css variables as well as class names
+import { rose } from "tailwindcss/colors";
+// import gunmetal from "@tailwindcss-gunmetal/colors";
 
-console.log(colors);
+console.log("nothing");
+
+// type Colors = {
+//     gunmetal: {
+//         50: string;
+//         100: string;
+//         200: string;
+//         300: string;
+//         400: string;
+//         500: string;
+//         600: string;
+//         700: string;
+//         800: string;
+//         900: string;
+//         950: string;
+//     };
+// };
+
+// const gunmetal = gunmetalPlugin.config?.theme?.extend?.colors as Colors;
+// const colors = gunmetal.gunmetal;
+
+// export const shadcn = {
+//     background: colors[950],
+//     foreground: colors[50],
+//
+//     card: {
+//         DEFAULT: colors[950],
+//         foreground: colors[50],
+//     },
+//
+//     popover: {
+//         DEFAULT: colors[950],
+//         foreground: colors[50],
+//     },
+//
+//     primary: {
+//         DEFAULT: colors[50],
+//         foreground: colors[900],
+//     },
+//
+//     secondary: {
+//         DEFAULT: colors[800],
+//         foreground: colors[50],
+//     },
+//
+//     muted: {
+//         DEFAULT: colors[800],
+//         foreground: colors[400],
+//     },
+//
+//     accent: {
+//         DEFAULT: colors[800],
+//         foreground: colors[50],
+//     },
+//
+//     destructive: {
+//         DEFAULT: rose[900],
+//         foreground: rose[100],
+//     },
+//
+//     border: colors[800],
+//     input: colors[800],
+//     ring: colors[300],
+// };
